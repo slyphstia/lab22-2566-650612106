96af243d105e8c7d6cd51a75cd93dfd20f667de7
hghgh <apiwit_boonyarit@cmu.ac.th>
2023-09-28 15:33:31 +0700

    cp

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650612106 "
+                fullName="Apiwit Boonyarit"
                 year="2023"
               />
             </Container>
